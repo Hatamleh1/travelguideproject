f34ac9d7310069cc897980ee0eadfa0a8f587bd5
Mathilda <mathildabackstrom@hotmail.se>
2020-04-14 12:39:39 +0200

    START REAL CHANGES

--- Final project.py
+++ Final project.py
@@ -1,6 +1,30 @@
 from tkinter import *
-from tkinter.colorchooser import askcolor
-from tkinter import ttk
+import tkinter as ttk
+
 
 root = Tk()
-root.title('Travel guide')
+root.title("Tk dropdown example")
+
+mainframe = Frame(root)
+mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
+mainframe.columnconfigure(0, weight = 1)
+mainframe.rowconfigure(0, weight = 1)
+mainframe.pack(pady = 100, padx = 100)
+
+country_choice = StringVar(root)
+
+choices = {'Holland', 'Spain', 'Germany', 'UK', 'France'}
+country_choice.set('Spain')
+
+popupMenu = OptionMenu(mainframe, country_choice, *choices)
+Label(mainframe, text="Choose a country").grid(row=1, column=1)
+popupMenu.grid(row=2, column=1)
+
+
+def change_dropdown(*args):
+    print(country_choice.get())
+
+
+country_choice.trace('w', change_dropdown)
+
+root.mainloop()
