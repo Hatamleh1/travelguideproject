72ed535f72acc3e5e27e60d07b84e714734cc277
Mathilda <mathildabackstrom@hotmail.se>
2020-04-17 17:31:30 +0200

    Creating button and classes

--- Final project.py
+++ Final project.py
@@ -1,58 +1,78 @@
+import tkinter as tk
 from tkinter import *
-import tkinter as ttk
-from firebase import firebase, pyerbase
-from getpass import getpass
-
-firebaseConfig = {
-    "apiKey": "AIzaSyCgsTaRzssS9JsgC88uTDGoFS2TchT_kr4",
-    "authDomain": "travelguideproject-3d198.firebaseapp.com",
-    "databaseURL": "https://travelguideproject-3d198.firebaseio.com",
-    "projectId": "travelguideproject-3d198",
-    "storageBucket": "travelguideproject-3d198.appspot.com",
-    "messagingSenderId": "1038280136856",
-    "appId": "1:1038280136856:web:d0be96210c16ed0f6998ed",
-    "measurementId": "G-SGY74C7BSY"
-}
 
 
-firebase = pyrebase.initialize_app(firebaseConfig)
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+       tk.Frame.__init__(self, *args, **kwargs)
 
-auth = firebase.auth()
+    def show(self):
+        self.lift()
 
-email = input("Email:\n")
-password = getpass("Password:\n")
+##### Here Laith code
 
-user = auth.create_user_with_email_and_password(email, password)
+class Page1(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, bg='white')
+       label.pack(side="top", fill="both", expand=True)
 
-
-
-
-root = Tk()
-root.title("Tk dropdown example")
-
-mainframe = Frame(root)
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-mainframe.pack(pady=100, padx=100)
-
-country_choice = StringVar(root)
-
-choices = {'Holland', 'Spain', 'Germany', 'UK', 'France'}
-country_choice.set('Select country')
-
-popupMenu = OptionMenu(mainframe, country_choice, *choices)
-Label(mainframe, text="Choose a country").grid(row=1, column=1)
-popupMenu.grid(row=2, column=1)
+       country_choice = StringVar(root)
+       choices = {'Holland', 'Spain', 'Germany', 'UK', 'France'}
+       country_choice.set('Select country')
+       popupMenu = OptionMenu(label, country_choice, *choices)
+       Label(label, text="Choose a country").place(relx=0.5, rely=0.4, anchor=CENTER)
+       popupMenu.place(relx=0.5, rely=0.5, anchor=CENTER)
+       B1 = Button(label, text="Continue", command=Page2)
+       B1.place(relx=0.5, rely=0.6, anchor=CENTER)
 
 
-def change_dropdown(*args):
-    print(country_choice.get())
+class Page2(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 2")
+       label.pack(side="top", fill="both", expand=True)
 
 
-country_choice.trace('w', change_dropdown)
+class Page3(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 3")
+       label.pack(side="top", fill="both", expand=True)
 
-change_dropdown() == ''
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page1(self)
+        p2 = Page2(self)
+        p3 = Page3(self)
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(buttonframe, text="Page 1", command=p1.lift)
+        b2 = tk.Button(buttonframe, text="Page 2", command=p2.lift)
+        b3 = tk.Button(buttonframe, text="Page 3", command=p3.lift)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+
+        p1.show()
 
 
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('Our TripAdvisor')
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("400x400")
+    root.mainloop()
+
