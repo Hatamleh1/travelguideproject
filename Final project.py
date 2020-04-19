eaa0f5c6d42b81b1b85f643d9df4f651cdc362d7
Mathilda <mathildabackstrom@hotmail.se>
2020-04-19 14:00:58 +0200

    updates

--- Final project.py
+++ Final project.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from tkinter import *
+import pickle
 
+accounts = {}
 
 # This code runs the pages, places root and shows different frames.
-
 class TravelguideApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
+        self.title('TravelguideApp')
         self.geometry('250x300')
         container = tk.Frame(self)
 
@@ -15,16 +17,95 @@ class TravelguideApp(tk.Tk):
         container.pack(side='top')
 
         self.frames = {}
-        for F in (main_page, holland, spain, germany, UK, france, LoginPage, about_holland, best_visit, currency, electricity, transport, city, amsterdam_nav, gudie_info, hotels, restaurants, sightseeing, activities):
+        for F in (main_page, holland, spain, germany, UK, france, LoginPage, SignupPage, about_holland, best_visit, currency, electricity, transport, city, amsterdam_nav, gudie_info, hotels, restaurants, sightseeing, activities):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky=(N, S, E, W))
 
-        self.show_frame(main_page)
+        self.show_frame(LoginPage)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
+
+
+# This is our login function.
+# The page of login
+
+class LoginPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg='light grey', width=500, height=500)
+
+
+        userLbl = Label(self, text="Username:", bg='light grey')
+        userLbl.place(relx=0.5, rely=0.4, anchor=CENTER)
+        userEnt = Entry(self, text='', highlightbackground='light grey')
+        userEnt.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        passLbl = Label(self, text="Password:", bg='light grey')
+        passLbl.place(relx=0.5, rely=0.6, anchor=CENTER)
+        passEnt = Entry(self, text='', show='*', highlightbackground='light grey')
+        passEnt.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+        loginBtn = Button(self, text="Login", command=lambda:CheckLogin(), highlightbackground='light grey')
+        loginBtn.place(relx=0.56, rely=0.8, anchor=CENTER)
+
+        signupBtn = Button(self, text='Sign Up', highlightbackground='light grey', command=lambda: controller.show_frame(SignupPage))
+        signupBtn.place(relx=0.4, rely=0.8, anchor=CENTER)
+
+        # Checks if login is correct.
+
+        def CheckLogin():
+            upload = open('accounts.pickle', 'rb')
+            accounts = pickle.load(upload)
+
+            if userEnt.get() in accounts and accounts[userEnt.get()] == passEnt.get():
+                controller.show_frame(main_page)
+                r = Tk() # Opens new window
+                r.title(':D')
+                r.geometry('150x50') # Makes the window a certain size
+                rlbl = Label(r, text='\n[+] Logged In') # "logged in" label
+                rlbl.pack() # Pack is like .grid(), just different
+                r.mainloop()
+            else:
+                r = Tk()
+                r.title('D:')
+                r.geometry('150x50')
+                rlbl = Label(r, text='\n[!] Invalid Login')
+                rlbl.pack()
+                r.mainloop()
+
+# Creates signup page.
+
+class SignupPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg='light grey', width=500, height=500)
+        global new_passEnt
+        global new_userEnt
+
+        new_userLbl = Label(self, text="Create Username:", bg='light grey')
+        new_userLbl.place(relx=0.5, rely=0.4, anchor=CENTER)
+        new_userEnt = Entry(self, text='', highlightbackground='light grey')
+        new_userEnt.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        new_passLbl = Label(self, text="Create Password:", bg='light grey')
+        new_passLbl.place(relx=0.5, rely=0.6, anchor=CENTER)
+        new_passEnt = Entry(self, text='', show='*', highlightbackground='light grey')
+        new_passEnt.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+
+        submitBtn = Button(self, text='Submit', command=lambda: FSSignup(), highlightbackground='light grey')
+        submitBtn.place(relx=0.4, rely=0.8, anchor=CENTER)
+
+        cancelBtn = Button(self, text='Cancel', command=lambda: controller.show_frame(LoginPage), highlightbackground='light grey')
+        cancelBtn.place(relx=0.56, rely=0.8, anchor=CENTER)
+
+
+        def FSSignup():
+            accounts[new_userEnt.get()] = new_passEnt.get()
+            download = open('accounts.pickle', 'wb')
+            pickle.dump(accounts, download)
+            download.close()
 
 
 # This is the first page, choosing a country.
@@ -96,8 +177,8 @@ class holland(tk.Frame):
 # Page about Holland
 class about_holland(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        label = Label(self, text="About Holland", bg='white')
+        Frame.__init__(self, parent, bg='light grey')
+        label = Label(self, text="Here you can get all information \n about Holland needed before \n your trip to Holland.", bg='light grey')
         label.pack(side="top", fill="both", expand=True)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(holland), highlightbackground='light grey', width=5)
@@ -110,8 +191,8 @@ class about_holland(tk.Frame):
 # Best time to visit.
 class best_visit(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        label = Label(self, text="Best time to visit", bg='white')
+        Frame.__init__(self, parent, bg='light grey')
+        label = Label(self, text="Here you can read about the best \n time of the year to visit Holland", bg='light grey')
         label.pack(side="top", fill="both", expand=True)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(holland), highlightbackground='light grey', width=5)
@@ -124,8 +205,8 @@ class best_visit(tk.Frame):
 # Transportation information about Holland.
 class transport(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        label = Label(self, text="Public transportation", bg='white')
+        Frame.__init__(self, parent, bg='light grey')
+        label = Label(self, text="Here you can get information about \n Hollands pubic transportation", bg='light grey')
         label.pack(side="top", fill="both", expand=True)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(holland), highlightbackground='light grey', width=5)
@@ -138,8 +219,8 @@ class transport(tk.Frame):
 # Currency information about Holland.
 class currency(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        label = Label(self, text="Currency", bg='white')
+        Frame.__init__(self, parent, bg='light grey')
+        label = Label(self, text="What currency does Holland have? \n Here you can find out. ", bg='light grey')
         label.pack(side="top", fill="both", expand=True)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(holland), highlightbackground='light grey', width=5)
@@ -152,8 +233,8 @@ class currency(tk.Frame):
 # Electricity information about Holland.
 class electricity(tk.Frame):
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        label = Label(self, text="Electricity", bg='white')
+        Frame.__init__(self, parent, bg='light grey')
+        label = Label(self, text="Holland has this typ of electricity.", bg='light grey')
         label.pack(side="top", fill="both", expand=True)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(holland), highlightbackground='light grey', width=5)
@@ -230,7 +311,7 @@ class amsterdam_nav(tk.Frame):
 class gudie_info(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='light grey', width=500, height=500)
-        label = Label(self, text="Guide information", bg="light grey")
+        label = Label(self, text="Guide information about Amsterdam.", bg="light grey")
         label.place(relx=0.5, rely=0.4, anchor=CENTER)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(amsterdam_nav), highlightbackground='light grey', width=5)
@@ -243,28 +324,109 @@ class gudie_info(tk.Frame):
 class hotels(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='light grey', width=500, height=500)
-        label = Label(self, text="Hotels", bg="light grey")
-        label.place(relx=0.5, rely=0.4, anchor=CENTER)
+        label = Label(self, text="Find hotels and their ratings.", bg="light grey")
+        label.place(relx=0.5, rely=0.1, anchor=CENTER)
+
+        o1 = tk.Entry(self, width=9, highlightbackground='light grey')
+        o2 = tk.Entry(self, width=9, highlightbackground='light grey')
+        o3 = tk.Entry(self, width=9, highlightbackground='light grey')
+        o4 = tk.Entry(self, width=9, highlightbackground='light grey')
+
+        def display():
+            tot = 0
+            if o1.get() == "5":
+                tk.Label().place(relx=0.82, rely=0.35, anchor=W)
+                tot += 5
+            if o1.get() == "4":
+                tk.Label(self).place(relx=0.82, rely=0.35, anchor=W)
+                tot += 4
+            if o1.get() == "3":
+                tk.Label(self).place(relx=0.82, rely=0.35, anchor=W)
+                tot += 3
+            if o1.get() == "2":
+                tk.Label(self).place(relx=0.82, rely=0.35, anchor=W)
+                tot += 2
+            if o1.get() == "1":
+                tk.Label(self).place(relx=0.82, rely=0.35, anchor=W)
+                tot += 1
+            if o2.get() == "5":
+                tk.Label(self).place(relx=0.82, rely=0.45, anchor=W)
+                tot += 5
+            if o2.get() == "4":
+                tk.Label(self).place(relx=0.82, rely=0.45, anchor=W)
+                tot += 4
+            if o2.get() == "3":
+                tk.Label(self).place(relx=0.82, rely=0.45, anchor=W)
+                tot += 3
+            if o2.get() == "2":
+                tk.Label(self).place(relx=0.82, rely=0.45, anchor=W)
+                tot += 2
+            if o2.get() == "1":
+                tk.Label(self).place(relx=0.82, rely=0.45, anchor=W)
+                tot += 1
+            if o3.get() == "5":
+                tk.Label(self).place(relx=0.82, rely=0.55, anchor=W)
+                tot += 5
+            if o3.get() == "4":
+                tk.Label(self).place(relx=0.82, rely=0.55, anchor=W)
+                tot += 4
+            if o3.get() == "3":
+                tk.Label(self).place(relx=0.82, rely=0.55, anchor=W)
+                tot += 3
+            if o3.get() == "2":
+                tk.Label(self).place(relx=0.82, rely=0.55, anchor=W)
+                tot += 2
+            if o3.get() == "1":
+                tk.Label(self).place(relx=0.82, rely=0.55, anchor=W)
+                tot += 1
+            if o4.get() == "5":
+                tk.Label(self).place(relx=0.82, rely=0.65, anchor=W)
+                tot += 5
+            if o4.get() == "4":
+                tk.Label(self).place(relx=0.82, rely=0.65, anchor=W)
+                tot += 4
+            if o4.get() == "3":
+                tk.Label(self).place(relx=0.82, rely=0.65, anchor=W)
+                tot += 3
+            if o4.get() == "2":
+                tk.Label(self).place(relx=0.82, rely=0.65, anchor=W)
+                tot += 2
+            if o4.get() == "1":
+                tk.Label(self, highlightbackground='light grey', text='1').place(relx=0.82, rely=0.65, anchor=W)
+                tot += 1
+
+            #tk.Label(self, text=str(tot), bg='light grey').place(relx=0.76, rely=0.35, anchor=W)
+            #tk.Label(self, text=str(tot/15), bg='light grey').place(relx=0.76, rely=0.35, anchor=W)
+
+        tk.Label(self, text="Hotels", font=('Helvetica', 13, 'bold'), bg='light grey').place(relx=0.03, rely=0.25, anchor=W)
+        tk.Label(self, text="Wellington", bg='light grey').place(relx=0.03, rely=0.35, anchor=W)
+        tk.Label(self, text="W part hotel", bg='light grey').place(relx=0.03, rely=0.45, anchor=W)
+        tk.Label(self, text="Ibbys Hotel", bg='light grey').place(relx=0.03, rely=0.55, anchor=W)
+        tk.Label(self, text="Yellow Hotel", bg='light grey').place(relx=0.03, rely=0.65, anchor=W)
+
+        tk.Label(self, text="Your opinion", font=('Helvetica', 13, 'bold'), bg='light grey').place(relx=0.37, rely=0.25, anchor=W)
+        o1.place(relx=0.37, rely=0.35, anchor=W)
+        o2.place(relx=0.37, rely=0.45, anchor=W)
+        o3.place(relx=0.37, rely=0.55, anchor=W)
+        o4.place(relx=0.37, rely=0.65, anchor=W)
+
+        tk.Label(self, text="Rating", font=('Helvetica', 13, 'bold'), bg='light grey').place(relx=0.76, rely=0.25, anchor=W)
+
+        button1 = tk.Button(self, text="submit", command=display, highlightbackground='light grey')
+        button1.place(relx=0.41, rely=0.70)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(amsterdam_nav), highlightbackground='light grey', width=5)
         back_btn.place(relx=0.2, rely=0.9, anchor=CENTER)
 
         logout = Button(self, text="Logout", bg='light grey', highlightbackground='light grey', command=lambda: controller.show_frame(LoginPage), width=5)
         logout.place(relx=0.8, rely=0.9, anchor=CENTER)
-        rating1 = Scale(self, from_=0, to=5, orient=HORIZONTAL)
-        rating1.place(relx=0.75, rely=0.3, anchor=CENTER)
-        rating2 = Scale(self, from_=0, to=5, orient=HORIZONTAL)
-        rating2.place(relx=0.75, rely=0.45, anchor=CENTER)
-        rating3 = Scale(self, from_=0, to=5, orient=HORIZONTAL)
-        rating3.place(relx=0.75, rely=0.6, anchor=CENTER)
-        rating4 = Scale(self, from_=0, to=5, orient=HORIZONTAL)
-        rating4.place(relx=0.75, rely=0.75, anchor=CENTER)
+
 
 
 class restaurants(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='light grey', width=500, height=500)
-        label = Label(self, text="Restaurants", bg="light grey")
+        label = Label(self, text="Here you can find the best \n restaurants.", bg="light grey")
         label.place(relx=0.5, rely=0.4, anchor=CENTER)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(amsterdam_nav), highlightbackground='light grey', width=5)
@@ -277,7 +439,7 @@ class restaurants(tk.Frame):
 class sightseeing(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='light grey', width=500, height=500)
-        label = Label(self, text="Sightseeing", bg="light grey")
+        label = Label(self, text="Excited to go on some sightseeing. \n Anne franks museum is rated highly \n so don't miss it.", bg="light grey")
         label.place(relx=0.5, rely=0.4, anchor=CENTER)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(amsterdam_nav), highlightbackground='light grey', width=5)
@@ -290,7 +452,7 @@ class sightseeing(tk.Frame):
 class activities(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='light grey', width=500, height=500)
-        label = Label(self, text="Activities", bg="light grey")
+        label = Label(self, text="Find the most interesting activities \n to do in Amsterdam. ", bg="light grey")
         label.place(relx=0.5, rely=0.4, anchor=CENTER)
 
         back_btn = Button(self, text='Back', command=lambda: controller.show_frame(amsterdam_nav), highlightbackground='light grey', width=5)
@@ -381,6 +543,7 @@ class LoginPage(tk.Frame):
         signupBtn.place(relx=0.4, rely=0.8, anchor=CENTER)
 
 
-#call and run code
+# call and run code
 app = TravelguideApp()
 app.mainloop()
+
