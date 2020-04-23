d030f11e764dc0584b5075c25457de9a44568a44
Laith <laith.hatamleh7@gmail.com>
2020-04-23 22:52:06 +0200

    gg2

--- populator.py
+++ populator.py
@@ -3,7 +3,7 @@ import pickle
 
 file = open('hotel_dict', 'wb')
 
-d = {"Amsterdam": [Hotel("HOTEL V NESPLEIN"), Hotel("MOTEL ONE AMSTERDAM WATERLOOPLEIN"), Hotel("HYATT REGENCY"), Hotel("ESTHEREA")],
+d = {"Amsterdam": [Hotel("HOTEL V NESPLEIN"), Hotel("MOTEL ONE WATERLOOPLEIN"), Hotel("HYATT REGENCY"), Hotel("ESTHEREA")],
      "Haag": [Hotel("Hague1 Hotel"), Hotel("Hague2 Hotel"), Hotel("Hague3 Hotel"), Hotel("Hague4 Hotel")],
      "Rotterdam": [Hotel("Rotterdam Hotel"), Hotel("Rotterdam2 Hotel"), Hotel("Rotterdam3 Hotel"), Hotel("Rotterdam4 Hotel")],
      "Best": [Hotel("Best Hotel"), Hotel("Best2 Hotel"), Hotel("Best3 Hotel"), Hotel("Best4 Hotel")]}
